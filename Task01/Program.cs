62c6678d71e1d5a9ba7dd5343b31880c96a54071
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-13 20:42:34 +0300

    Задача №1 решена

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введи пятизначное число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        string numText = Convert.ToString(num);
+        if (numText.Length > 5 || numText.Length < 5)
+        {
+            Console.WriteLine("Число должно быть пятизначным.");
+        }
+        else if (numText[0] == numText[4] && numText[1] == numText[3])
+        {
+            Console.WriteLine($"Число {num} является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine($"Число {num} не является палиндромом.");
+        }
+    }
+}
