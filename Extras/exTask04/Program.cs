f792880ffb7c00a1a5e5a87f6a8ab7879cbe3801
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-14 20:39:23 +0300

    Оформелние кода по стандарту С#

--- Extras/exTask04/Program.cs
+++ Extras/exTask04/Program.cs
@@ -1,12 +1,18 @@
-﻿//Задача 4. Дан массив длиной 10 элементов. Заполнить его числами от 1 до 10.
-Random rnd = new Random();
-int value=rnd.Next(1,10);
-int [] array = {rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10)};
-int i=0;
-Console.Write("[ ");
-while (i<10)
+﻿internal partial class Program
 {
-    Console.Write(array[i]+" ");
-    i++;
+    private static void Main(string[] args)
+    {
+        //Задача 4. Дан массив длиной 10 элементов. Заполнить его числами от 1 до 10.
+        Random rnd = new Random();
+        int value = rnd.Next(1, 10);
+        int[] array = { rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10) };
+        int i = 0;
+        Console.Write("[ ");
+        while (i < 10)
+        {
+            Console.Write(array[i] + " ");
+            i++;
+        }
+        Console.Write("]");
+    }
 }
-Console.Write("]");
