d47282b4bd77131becab67b32911d5d924175c16
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-14 19:58:53 +0300

    Дополнительная задача №2 решена

--- Extras/exTask02/Program.cs
+++ Extras/exTask02/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задача 2. Дано трёх значное число N. Определить кратна ли трём сумма всех его цифр.
+        Console.Write("Введите трехзначное число: ");
+        int numeric = Convert.ToInt32(Console.ReadLine());
+        if (numeric > 99 && numeric < 1000)
+        {
+            int number1 = numeric / 100;
+            int number2 = numeric / 10 % 10;
+            int number3 = numeric % 10;
+            if ((number1 + number2 + number3) % 3 == 0)
+            {
+                Console.WriteLine($"Сумма цифр числа {numeric} кратна 3.");
+            }
+            else
+            {
+                Console.WriteLine($"Сумма цифр числа {numeric} некратна 3.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Число должно быть трехзначным.");
+        }
+    }
+}
