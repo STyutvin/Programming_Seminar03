23efe8aca7453e667beec620e62e57d15bc6ad1a
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-14 20:37:46 +0300

    Дополнительная задача №4 решена

--- Extras/exTask04/Program.cs
+++ Extras/exTask04/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4. Дан массив длиной 10 элементов. Заполнить его числами от 1 до 10.
+Random rnd = new Random();
+int value=rnd.Next(1,10);
+int [] array = {rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10),rnd.Next(1,10)};
+int i=0;
+Console.Write("[ ");
+while (i<10)
+{
+    Console.Write(array[i]+" ");
+    i++;
+}
+Console.Write("]");
