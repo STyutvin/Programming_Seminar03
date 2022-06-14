5be6c0c4b9ef3513afaa835c6935a2e5dc4b410c
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-14 20:41:40 +0300

    Убрал неиспользуемую переменную value

--- Extras/exTask04/Program.cs
+++ Extras/exTask04/Program.cs
@@ -4,7 +4,6 @@
     {
         //Задача 4. Дан массив длиной 10 элементов. Заполнить его числами от 1 до 10.
         Random rnd = new Random();
-        int value = rnd.Next(1, 10);
         int[] array = { rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10), rnd.Next(1, 10) };
         int i = 0;
         Console.Write("[ ");
