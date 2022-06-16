19d25588ca6127eb3518d7a1289f8655c01879b6
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-16 20:42:03 +0300

    Задача №8 решена

--- Brain_Breaking_Tasks/bbTask08/Program.cs
+++ Brain_Breaking_Tasks/bbTask08/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8. Напишите программу, который выводит на консоль таблицу умножения от 1 до n, где n задаётся случайно от 2 до 100.
+Random rnd = new Random();
+int n=rnd.Next(2,100);
+for (int i=0;i<10;i++)
+{
+    Console.Write($"{n} x {i} = {n*i}");
+    Console.WriteLine();
+}
+//Данная задача была в разделе задач повышенной сложности... Надеюсь, что условие задачи я понял правильно.
