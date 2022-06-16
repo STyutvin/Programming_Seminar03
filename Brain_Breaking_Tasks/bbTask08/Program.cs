68d3117a1323f421672b11ae629c477d4044f9d0
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-16 20:43:57 +0300

    Оформление кода в соответствии C#

--- Brain_Breaking_Tasks/bbTask08/Program.cs
+++ Brain_Breaking_Tasks/bbTask08/Program.cs
@@ -1,9 +1,15 @@
-﻿//Задача 8. Напишите программу, который выводит на консоль таблицу умножения от 1 до n, где n задаётся случайно от 2 до 100.
-Random rnd = new Random();
-int n=rnd.Next(2,100);
-for (int i=0;i<10;i++)
+﻿internal class Program
 {
-    Console.Write($"{n} x {i} = {n*i}");
-    Console.WriteLine();
+    private static void Main(string[] args)
+    {
+        //Задача 8. Напишите программу, который выводит на консоль таблицу умножения от 1 до n, где n задаётся случайно от 2 до 100.
+        Random rnd = new Random();
+        int n = rnd.Next(2, 100);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"{n} x {i} = {n * i}");
+            Console.WriteLine();
+        }
+    }
 }
 //Данная задача была в разделе задач повышенной сложности... Надеюсь, что условие задачи я понял правильно.
